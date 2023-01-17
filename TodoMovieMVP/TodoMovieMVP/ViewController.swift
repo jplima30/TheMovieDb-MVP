d9a390f79170426e126e97f5122f715d74cb64f4
Joao Paulo Lima Silva <joao_silva_ext_7@carrefour.com>
2023-01-16 23:54:20 -0300

    Estrutura do projeto e pods

--- TodoMovieMVP/TodoMovieMVP/ViewController.swift
+++ TodoMovieMVP/TodoMovieMVP/ViewController.swift
@@ -2,25 +2,45 @@
 //  ViewController.swift
 //  TodoMovieMVP
 //
-//  Created by Joao Paulo Lima Silva on 10/01/23.
+//  Created by Joao Paulo Lima Silva on 13/01/23.
 //
 
-import Cocoa
+import UIKit
+import SwiftUI
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
+private var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.backgroundColor = .clear
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.rowHeight = UITableView.automaticDimension
+    return tableView
+}()
+
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+    }
+}
